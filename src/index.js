652f1d1083be86d5ff2f8ff472e66812458caebe
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2022-11-12 12:08:09 +0300

    mini project bloggers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const Blogger = () => {
+  return (
+    <div className='main-container'>
+      <Avatar />
+      <BloggerName />
+      <Bio />
+    </div>
+  );
+};
+const Avatar = (props) => {
+  console.log(props);
+  return (
+    <div className='image-container'>
+      <img
+        src='https://pickaface.net/gallery/avatar/acrovin559439058dc7f.png'
+        alt='avatar'
+      />
+    </div>
+  );
+};
+const BloggerName = () => {
+  return (
+    <div className='blogger-name-container'>
+      <p>Сергей Иванов!!!</p>
+    </div>
+  );
+};
+const Bio = () => {
+  return (
+    <div className='bio'>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt rerum
+        amet, doloremque sint voluptate pariatur perferendis. Iusto quae eos,
+        totam facilis quasi voluptatibus voluptas quos repellendus dolor. Natus,
+        modi et!
+      </p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.Fragment>
-    <h1>Hello world!!! My first app</h1>
-  </React.Fragment>
+  <div className='root'>
+    <Blogger />
+    <Blogger />
+    <Blogger />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
