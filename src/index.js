2aa2dd69141137dd636885f0ab48d40d49547663
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2022-11-15 20:39:46 +0300

    completed first project js only

--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+const bloggers = [
+  {
+    name: "Сергей Иванов",
+    avatar: "https://pickaface.net/gallery/avatar/acrovin559439058dc7f.png",
+    bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt rerum ametvoluptatibus voluptas quos repellendus dolor. Natus, modi et!",
+  },
+  {
+    name: "Петр Петров",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3MMleJTsvxXrWLqg8XEeMcVgRkFu1okrTaQ&usqp=CAU",
+    bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Иван Иванов",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM37K_9ZjynSqoSDA0Nyl2oqIZRV6p4xfVxvd-57q19ZJDLCB23qhHF0zwqLkycRplQ-A&usqp=CAU",
+    bio: "Lorem ipsum dolor sit, 333333",
+  },
+];
 
-const Blogger = () => {
+const Blogger = (props) => {
+  const { avatar, name, bio } = props;
   return (
     <div className='main-container'>
-      <Avatar />
-      <BloggerName />
-      <Bio />
+      <Avatar url={avatar} />
+      <BloggerName name={name} />
+      <Bio bio={bio} />
     </div>
   );
 };
-const Avatar = (props) => {
-  console.log(props);
+const Avatar = ({ url }) => {
   return (
     <div className='image-container'>
-      <img
-        src='https://pickaface.net/gallery/avatar/acrovin559439058dc7f.png'
-        alt='avatar'
-      />
+      <img src={url} alt='avatar' />
     </div>
   );
 };
-const BloggerName = () => {
+const BloggerName = ({ name }) => {
   return (
     <div className='blogger-name-container'>
-      <p>Сергей Иванов!!!</p>
+      <p>{name}</p>
     </div>
   );
 };
-const Bio = () => {
+const Bio = ({ bio }) => {
   return (
     <div className='bio'>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt rerum
-        amet, doloremque sint voluptate pariatur perferendis. Iusto quae eos,
-        totam facilis quasi voluptatibus voluptas quos repellendus dolor. Natus,
-        modi et!
-      </p>
+      <p>{bio}</p>
     </div>
   );
 };
@@ -45,8 +56,8 @@ const Bio = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className='root'>
-    <Blogger />
-    <Blogger />
-    <Blogger />
+    {bloggers.map((blogger) => {
+      return <Blogger {...blogger} />;
+    })}
   </div>
 );
